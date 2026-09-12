7f5d969a6f48de84557782253f1f837fcf62c132
cvats <cvatss.com@gmail.com>
2026-09-12 12:53:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import path, getenv
 
 class config:
-    API_ID = int(getenv("33591348", ""))
-    API_HASH = getenv("8889285131:AAG_k3PygmP7IY7eJxeiq72BkSFBIDt3x4Y", "")
-    BOT_TOKEN = getenv("8889285131:AAG_k3PygmP7IY7eJxeiq72BkSFBIDt3x4Y", "")
-    IP_API =getenv("7aecc8ed745d0c","")
+    API_ID = int(getenv("", ""))
+    API_HASH = getenv(":", "")
+    BOT_TOKEN = getenv("8889285131:", "")
+    IP_API =getenv("","")
     
 con = config()
